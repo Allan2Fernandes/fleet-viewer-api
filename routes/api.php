30c1fb5767728d2a0ee4687741799ca44e0573b9
Allan2Fernandes <allan2fernandes@hotmail.com>
2026-09-03 15:29:09 +0530

    Added test endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
 require __DIR__ . '/auth.php';
 require __DIR__ . '/robots.php';
 require __DIR__ . '/events.php';
+
+Route::get('deployment-test', fn() => response()->json(['message' => 'Response from Backend']));
