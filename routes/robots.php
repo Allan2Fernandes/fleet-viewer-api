2ce4e27c7e6cdc9ef3f2445ff11147bf8e7452af
Allan2Fernandes <allan2fernandes@hotmail.com>
2026-09-03 20:32:10 +0530

    attempt #3

--- routes/robots.php
+++ routes/robots.php
@@ -7,7 +7,7 @@ use App\Http\Controllers\Robots\GetRobotsLatestEventController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('robots')->group(function () {
-        Route::post('/', GetListOfRobotsController::class);
+        Route::get('get-all-robots', GetListOfRobotsController::class);
         Route::post('latest-positions', GetRobotsLatestEventController::class);
     });
 });
